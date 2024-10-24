9fbb0947330abce76926c31de9d19a5eda32d193
Matthieu Colombert <matthieu.colombert@gmail.com>
2024-10-24 10:48:57 +0200

    Add comments.

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route("/dataviz/all-data")
 def all_data_dataviz():
-    connexion = sqlite3.connect("data.db")
-    cursor = connexion.cursor()
+    """
+    Route avec le merge des quatre tables.
+    """
+    connexion = sqlite3.connect("data.db")  # Connexion à la base de données sqlite
+    cursor = connexion.cursor()  # Création de l'objet crusor pour envoyer des requêtes sql
     
     sql_request = f"""SELECT country, invoice_nb, invoice_date, quantity, description, price
                         FROM  customer ta
@@ -15,8 +18,8 @@ def all_data_dataviz():
                         INNER JOIN order_detail tc ON tb.id = tc.order_id
                         INNER JOIN product td ON tc.product_id = td.id
                         LIMIT 50"""
-    data = cursor.execute(sql_request).fetchall()
-    column_names = [description[0] for description in cursor.description]
+    data = cursor.execute(sql_request).fetchall()  # Récupération des données
+    column_names = [description[0] for description in cursor.description]  # Récupération des noms des colonnes
     connexion.close()
     
     return render_template("dataviz.html", data=data, 
@@ -24,11 +27,14 @@ def all_data_dataviz():
 
 @app.route("/dataviz/detail/<table>")
 def table_dataviz(table):
-    connexion = sqlite3.connect("data.db")
-    cursor = connexion.cursor()
+    """
+    Route pour affiche rles données par table.
+    """
+    connexion = sqlite3.connect("data.db")  # Connexion à la base de données sqlite
+    cursor = connexion.cursor()  # Création de l'objet crusor pour envoyer des requêtes sql
     sql_request = f"SELECT * FROM {table} LIMIT 50"
-    data = cursor.execute(sql_request).fetchall()
-    column_names = [description[0] for description in cursor.description]
+    data = cursor.execute(sql_request).fetchall()  # Récupération des données
+    column_names = [description[0] for description in cursor.description]  # Récupération des noms des colonnes
     connexion.close()
     
     return render_template("dataviz.html", data=data, 
